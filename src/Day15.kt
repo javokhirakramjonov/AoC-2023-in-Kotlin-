94074ed70d7dd3e1ecfe5cd6cc4aa52099354b41
Javokhir <javokhir.akramjonov@gmail.com>
2023-12-15 23:06:32 +0100

    day 14 is fully solved

--- src/Day15.kt
+++ src/Day15.kt
@@ -1,29 +1,51 @@
 fun main() {
 
-    fun extractWords(input: List<String>) : List<String> {
+    fun extractWords(input: List<String>): List<String> {
         return input
             .joinToString("")
             .split(",")
             .toList()
     }
 
-    fun nextHashValue(current: Int, char: Char) : Int {
+    fun nextHashValue(current: Int, char: Char): Int {
         return (current + char.code) * 17 % 256
     }
 
-    fun part1(input: List<String>): Int {
-        return extractWords(input)
-            .sumOf {
-                val initial = 0
+    fun hash(input: String): Int {
+        return input.fold(0) { acc, char ->
+            nextHashValue(acc, char)
+        }
+    }
 
-                it.fold(initial) { acc, char ->
-                    nextHashValue(acc, char)
-                }
-            }
+    fun part1(input: List<String>): Int {
+        return extractWords(input).sumOf(::hash)
     }
 
     fun part2(input: List<String>): Int {
-        return 0
+        val map = HashMap<Int, LinkedHashMap<String, Int>>()
+
+        extractWords(input).forEach {
+            val opIndex = it.indexOfFirst { !it.isLetterOrDigit() }
+
+            if (it[opIndex] == '=') {
+                val key = it.substring(0, opIndex)
+                val length = it.substring(opIndex + 1).toInt()
+                val hash = hash(key)
+
+                map.getOrPut(hash) { LinkedHashMap() }[key] = length
+            } else {
+                val key = it.substring(0, opIndex)
+                val hash = hash(key)
+
+                map.getOrDefault(hash, HashMap()).remove(key)
+            }
+        }
+
+        return map.entries.sumOf { keyValue ->
+            keyValue.key.plus(1) * keyValue.value.values.mapIndexed { index, value ->
+                index.plus(1) * value
+            }.sum()
+        }
     }
 
     val input = readInput("Day15")
