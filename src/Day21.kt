a461490e72f3d11137c52c8505b43d5ed18df881
Javokhir <javokhir.akramjonov@gmail.com>
2023-12-22 19:06:12 +0100

    day 21 is fully solved

--- src/Day21.kt
+++ src/Day21.kt
@@ -1,5 +1,4 @@
 import java.util.*
-import kotlin.collections.HashSet
 
 fun main() {
 
@@ -56,8 +55,61 @@ fun main() {
         }
     }
 
+    fun lagrange(points: List<Point>, x: Long): Long {
+        val x1 = points[0].x;
+        val y1 = points[0].y;
+        val x2 = points[1].x;
+        val y2 = points[1].y;
+        val x3 = points[2].x;
+        val y3 = points[2].y;
+
+        return (((x - x2) * (x - x3)) / ((x1 - x2) * (x1 - x3)) * y1 +
+                ((x - x1) * (x - x3)) / ((x2 - x1) * (x2 - x3)) * y2 +
+                ((x - x1) * (x - x2)) / ((x3 - x1) * (x3 - x2)) * y3)
+    }
+
     fun part2(input: List<String>): Long {
-        return 0
+
+        val n = 26501365
+        val width = input.first().length
+        val height = input.size
+        val cycle = (n / width).toLong()
+        val remainder = n % width
+
+        val xS = input.indexOfFirst { it.contains('S') }
+        val yS = input[xS].indexOf('S')
+
+        var set = setOf(Point(xS, yS))
+
+        val points = mutableListOf<Point>()
+
+        var step = 0
+
+        repeat(3) {
+            while(step ++ < it * width + remainder) {
+                val dx = listOf(-1, 0, 1, 0)
+                val dy = listOf(0, 1, 0, -1)
+
+                set = set
+                    .flatMap {
+                        dx
+                            .zip(dy) { x, y ->
+                                Point(x + it.x, y + it.y)
+                            }
+                            .filter {
+                                val newX = (it.x % height + height) % height
+                                val newY = (it.y % width + width) % width
+
+                                input[newX][newY] != '#'
+                            }
+                    }
+                    .toSet()
+            }
+
+            points += Point(it, set.size)
+        }
+
+        return lagrange(points, cycle)
     }
 
     val input = readInput("Day21")
